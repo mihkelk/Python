9c9b164191958c062158c97819083d3721628140
Mihkel <mihkelkuslapuu@gmail.com>
2013-01-14 07:13:44 +0200

    Hüppamine osaliselt korras.

--- meng/meng.py
+++ meng/meng.py
@@ -5,7 +5,7 @@ from pyglet.window import key
 x = 5
 y = 6
 
-window = pyglet.window.Window()
+window = pyglet.window.Window(vsync = False)
 pimage =  pyglet.image.load('lel.jpg')
 platform_imag = pyglet.image.load('platform.jpg')
 
@@ -14,38 +14,63 @@ window.push_handlers(keys)
 t = 1
 fps_display = pyglet.clock.ClockDisplay()
 image =  pyglet.image.load('lel.jpg')
+jemp = False
+
+def update(dt):
+    return 0
+
+pyglet.clock.schedule_interval(update, 1.0/60.0)
+pyglet.clock.set_fps_limit(80)   
 
 class player:
     def __init__(self, pos, image):
         self.pos=[pos[0],pos[1]]
         self.vel=[0,0]
         self.image = pimage
-        self.width = 125
-        self.height = 118
-        self.pp2 = [self.pos[0] + self.width, self.pos[1]]
-        self.pp3 = [self.pos[0], self.pos[1] - self.height]
+        self.width = 118
+        self.height = 125
+        self.pp2 = [self.pos[0], self.pos[1] + self.height]
+        self.pp3 = [self.pos[0], self.pp2[1] ]
         self.pp4 = [self.pp2[0], self.pp3[1]]
         
     def draw(self):
         self.image.blit(self.pos[0], self.pos[1])
         
+    def jemp(self):
+        self.vel[1] = min(-self.vel[1],-7)    
+        
         
     def update(self):
+        global jemp
         
-        self.pp2 = [self.pos[0] + self.width, self.pos[1]]
-        self.pp3 = [self.pos[0], self.pos[1] - self.height]
-        self.pp4 = [self.pp2[0], self.pp3[1]]
+        self.pp2 = [self.pos[0], self.pos[1] + self.height]
+        self.pp3 = [self.pos[0] + self.width, self.pp2[1] ]
+        self.pp4 = [self.pp3[0], self.pos[1]]
         
         self.pos[0] = (self.pos[0] + self.vel[0]) % window.width
+       
         
+        #if jemp == True:
+            #self.vel[1] = min(-self.vel[1],-7)
+        #else:
+        self.pos[1] += self.vel[1]
+                 
         self.vel[0] *= .90 #friction, h66rdej6ud
+        
         # POS [1] ON TEGELIKULT OBJEKTI ALUMISEST SERVAST, tee korda
         
-        if self.pos[0] >= platform.pp2[0] or self.pp2[0] <=  platform.pos[0] or self.pp3[1] > platform.pos[1] :
-            self.pos[1]-=0.5
-            print platform.pos
-            print platform.pp2
+        if self.pos[0] >= platform.pp3[0] or self.pp3[0] <=  platform.pos[0] or self.pp4[1] > platform.pp2[1] :        
+            #self.vel[1] = min(-self.vel[1],-1)
+            if self.vel[1] > -20:
+                self.vel[1] -= 0.5              
+        else:
+            self.vel[1] = 0
+            if self.pos[1] < platform.pp2[1]:
+                self.pos[1] = platform.pp2[1]  
+            
+        #self.vel[1] = min(-self.vel[1],10)    
         self.image.blit(self.pos[0], self.pos[1])      
+
 
 class room:
     def __init__(self, width, height):
@@ -67,25 +92,25 @@ class sprite:
     def update(self):
         #self.image.blit(-player_1.pos[0]+self.pos[0], player_1.pos[1]+self.pos[1]) 
         self.image.blit(self.pos[0], self.pos[1])
-        self.pp2 = [self.pos[0] + self.width, self.pos[1]]
-        self.pp3 = [self.pos[0], self.pos[1] - self.height]
-        self.pp4 = [self.pp2[0], self.pp3[1]]
+        self.pp2 = [self.pos[0], self.pos[1] + self.height]
+        self.pp3 = [self.pos[0] + self.width, self.pp2[1] ]
+        self.pp4 = [self.pp3[0], self.pos[1]]
         
 
 player_1=player([200, 0], pimage)
-print player_1.pp3[1]
 platform = sprite([50,0], platform_imag)
 
-print player_1.pos
-print player_1.pos[1]
-print player_1.pp3[1]
 #print platform.pos[0]
 #@window.event
 def controls():
+    global jemp
     if keys == {}:
         pass
     elif keys[key.W]:
-        player_1.pos[1]+=10
+        #player_1.pos[1]+=10
+        #player_1.vel[1] += 4
+        player_1.vel[1] +=2
+        
     elif keys[key.A]:
         #player_1.pos[0]-=10
         player_1.vel[0] -=1
@@ -102,14 +127,11 @@ def controls():
 def progress():
     pass    
  
-def update(dt):
-    return 0
 
- 
 @window.event            
 def on_draw():
-    print player_1.pos[1]
     global t
+    pyglet.clock.tick()
     window.clear()
     fps_display.draw()
     controls()
@@ -118,6 +140,9 @@ def on_draw():
     platform.update()
     pass
     t +=1
+    pyglet.graphics.draw(2, pyglet.gl.GL_POINTS,
+    ('v2i', (int(player_1.pp4[0]), int(player_1.pp4[1]), 30, 35))
+    )
 
 #@window.event       
 #def on_key_press(symbol, modifiers): 
@@ -132,7 +157,7 @@ def on_draw():
 #        
 #        pass   
    
-pyglet.clock.schedule_interval(update, 1/60.0)    
+#pyglet.clock.schedule_interval(update, 1/30.0)    
     #pass
     #pyglet.app.run()    
         
