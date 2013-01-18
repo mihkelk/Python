9590ef3bc577ddaaca38400821fb22bcf861d872
Mihkel Kuslapuu <mihkelkuslapuu@gmail.com>
2013-01-18 11:10:25 +0200

    Kokkupuudete kontroll mitme objektiga, liikuv vaade.

--- meng/meng.py
+++ meng/meng.py
@@ -5,6 +5,7 @@ from pyglet.window import key
 x = 5
 y = 6
 
+    
 window = pyglet.window.Window(vsync = False)
 pimage =  pyglet.image.load('lel.jpg')
 platform_imag = pyglet.image.load('platform.jpg')
@@ -13,9 +14,11 @@ keys = key.KeyStateHandler()
 window.push_handlers(keys)
 fps_display = pyglet.clock.ClockDisplay()
 image =  pyglet.image.load('lel.jpg')
+#collisions = []
 
 def update(dt):
     return 0
+
 
 pyglet.clock.schedule_interval(update, 1.0/60.0)
 pyglet.clock.set_fps_limit(80)   
@@ -30,44 +33,60 @@ class player:
         self.pp2 = [self.pos[0], self.pos[1] + self.height]
         self.pp3 = [self.pos[0], self.pp2[1]]
         self.pp4 = [self.pp2[0], self.pp3[1]]
-        self.on_ground = True
+        self.collision = False
+        self.collisions = []
         
     def draw(self):
-        self.image.blit(self.pos[0], self.pos[1])
+        #self.image.blit(self.pos[0], self.pos[1])
+        self.image.blit(room_1.view(self.pos)[0], room_1.view(self.pos)[1])
         
     def jemp(self):
-        if self.on_ground == True:
+        #if self.collision == "on_ground":
+        if "on_ground" in self.collisions:    
             self.vel[1] +=50
         
         
     def update(self):
+        #global offset
         
         self.pp2 = [self.pos[0], self.pos[1] + self.height]
         self.pp3 = [self.pos[0] + self.width, self.pp2[1] ]
         self.pp4 = [self.pp3[0], self.pos[1]]
         
         self.pos[0] = (self.pos[0] + self.vel[0]) #% window.width
-        self.pos[1] = (self.pos[1] + self.vel[1]) % window.height
+        self.pos[1] = (self.pos[1] + self.vel[1]) #% window.height
                  
         self.vel[0] *= .90 #friction, h66rdej6ud
+        if self.vel[1] > -10:
+            self.vel[1] -= 0.5 
         
         # POS [1] ON TEGELIKULT OBJEKTI ALUMISEST SERVAST, tee korda
         
-        if self.on_ground == True:        
-            self.vel[1] = 0
-            if self.pos[1] < platform.pp2[1]:
-                self.pos[1] = platform.pp2[1]                    
-        elif self.on_ground == False:
-            if self.vel[1] > -10:
-                self.vel[1] -= 0.5    
- 
-        self.image.blit(self.pos[0], self.pos[1])      
+        if "on_ground" in self.collisions:
+            self.vel[1] = 0    
 
-
+        if self.pos[0] - room_1.offset[0] < room_1.clearance[0]:
+            room_1.offset[0] = self.pos[0] - room_1.clearance[0]
+        if self.pp3[0] - room_1.offset[0] > room_1.clearance[1]:
+            room_1.offset[0] = self.pp3[0] - room_1.clearance[1]
+        
+        
+        
+        
+        
+        
+             
+        print self.collisions
+        self.collisions = []
 class room:
     def __init__(self, width, height):
         self.width = 0
         self.height = 0
+        self.clearance = [300, 300]
+        self.offset = [0,0]
+        
+    def view(self, pos):
+        return [pos[0] - self.offset[0], pos[1] - self.offset[1]]
         
 class sprite:
     def __init__(self, pos, image):
@@ -83,30 +102,44 @@ class sprite:
     
     def update(self):
         #self.image.blit(-player_1.pos[0]+self.pos[0], player_1.pos[1]+self.pos[1]) 
-        self.image.blit(self.pos[0], self.pos[1])
+        #self.image.blit(self.pos[0], self.pos[1])
         self.pp2 = [self.pos[0], self.pos[1] + self.height]
         self.pp3 = [self.pos[0] + self.width, self.pp2[1] ]
         self.pp4 = [self.pp3[0], self.pos[1]]
         
+        self.image.blit(room_1.view(self.pos)[0], room_1.view(self.pos)[1])
+        
+        collide(player_1, self)
+        
 def collide(ob1, ob2):
-    if ob1.pos[0] >= ob2.pp3[0] or ob1.pp3[0] <=  ob2.pos[0] or ob1.pp4[1] > ob2.pp2[1] :
-        ob1.on_ground = False
+    #global collisions
+    
+    if (ob1.pos[0] <= ob2.pp4[0] and ob1.pos[0] >= ob2.pos[0]  or ob1.pp4[0] >=  ob2.pos[0] and ob1.pp4[0] <= ob2.pp4[0]) and ob1.pp4[1] <= ob2.pp2[1] and ob1.pp2[1] > ob2.pp4[1] :
+        ob1.collision = "on_ground"
+        if ob1.pp4[1] < ob2.pp3[1]:
+            ob1.pos[1] = ob2.pp3[1]
+            #ob1.collision = "submerged"
     else:
-        ob1.on_ground = True   
+        ob1.collision = "false"
+        
+    #if no ""    
+    ob1.collisions.append(ob1.collision)
+           
         #print "lel"
 
 player_1=player([200, 0], pimage)
-platform = sprite([50,0], platform_imag)
+room_1 = room(5,5)
 platvorme = []
-#platvorme = 4
+platvormod = 5
 
-for i in range(4):
-    i = sprite([i,i], platform_imag)
+for i in range(platvormod):
     platvorme.append(i)
+    platvorme[i] = sprite([i * 200,i * 50], platform_imag)
     print platvorme 
     
 
 
+    
 
 #@window.event
 def controls():
@@ -137,21 +170,14 @@ def on_draw():
     controls()
     player_1.update()
     
-    collide(player_1, platform)
     player_1.draw()
-    platform.update()
-    
-    #1.update()
-    
-    
-   # for i in range(4):
-       # i.update()
-        #i.draw()
-    
-    #pass
-    pyglet.graphics.draw(2, pyglet.gl.GL_POINTS,
-    ('v2i', (int(player_1.pp4[0]), int(player_1.pp4[1]), 30, 35))
-    )
 
+    
+    
+    for i in range(4):
+        platvorme[i].update()
+
+    pyglet.graphics.draw(2, pyglet.gl.GL_POINTS,
+    ('v2i', (int(player_1.pp4[0]), int(player_1.pp4[1]), 30, 35)))
         
 pyglet.app.run()
