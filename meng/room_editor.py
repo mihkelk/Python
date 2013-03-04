016d3ed31894ecd8ace16e705364757bd2b218c6
mihkelk <mihkelk@192.168.204.244>
2013-03-04 09:50:55 +0200

    room editor poolik

---
+++ meng/room_editor.py
@@ -0,0 +1,27 @@
+
+import pyglet
+
+window = pyglet.window.Window(vsync = False)
+
+class image_storage:
+    def __init__(self):
+        self.i_platform = pyglet.resource.image('platform.jpg')
+        
+images = image_storage        
+        
+def on_mouse_press(x, y, button, modifiers):
+    
+    if button = 
+    
+    pass        
+        
+        
+@window.event            
+def on_draw():
+    
+    #print "lel"
+    
+    
+pyglet.app.run()
+
+    
