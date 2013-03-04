7abcea7d961c0f2534af52e7e3c58d8ea3713f0e
mihkelk <mihkelk@192.168.204.244>
2013-03-04 10:02:15 +0200

    Poolik tubade loomise programm

--- meng/room_editor.py
+++ meng/room_editor.py
@@ -5,22 +5,28 @@ window = pyglet.window.Window(vsync = False)
 
 class image_storage:
     def __init__(self):
-        self.i_platform = pyglet.resource.image('platform.jpg')
+        self.i_platform = pyglet.sprite.Sprite('platform.jpg')
         
 images = image_storage        
         
-def on_mouse_press(x, y, button, modifiers):
-    
-    if button = 
-    
-    pass        
-        
-        
+      
+#images.i_platform = pyglet.resource.image('platform.jpg')        
+images.i_platform = pyglet.sprite.Sprite(pyglet.image.load('platform.jpg'))        
 @window.event            
 def on_draw():
-    
+    images.i_platform.draw()
+    #on_mouse_press()
     #print "lel"
+
+@window.event  
+def on_mouse_press(x, y, button, modifiers):
     
+    if button == pyglet.window.mouse.LEFT:
+        images.i_platform.x = x
+        images.i_platform.y = y
+        #print "heh"
+    
+    pass      
     
 pyglet.app.run()
 
