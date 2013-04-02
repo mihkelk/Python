838d84c0e3be1b4a881630d3b84697faa96bc967
mihkel.kuslapuu <mihkel.kuslapuu@ameerika1.LEH.local>
2013-04-02 13:19:29 +0300

    Muudatused.

--- praktika/GUI.py
+++ praktika/GUI.py
@@ -14,25 +14,20 @@ master = Tk()
 group = Label(master)
 group.pack(padx=10, pady=0, fill=X)
 
-lahtrid = []
-lahter = []
-#e = Label(master, text="Hello, world!")
-
 for i in range(active_table.k6rgus):
-    #lahtrid.append("1")
-    #lahter.append("1")
-  
+    
     if i %2:
         verv="white"
     else:
         verv="gray"
     Label(group, text=active_table.read[i].printer, bg=verv).grid(row=i, sticky=W+E, padx = 1)
-    
-for i in range(active_table.k6rgus):    
-    if i %2:
-        verv="white"
-    else:
-        verv="gray"
+    Label(group, text=active_table.read[i].tooner, bg=verv).grid(row=i, column=2, sticky=W+E, padx = 1)
+    Label(group, text=active_table.read[i].tulp_c, bg=verv).grid(row=i, column=3, sticky=W+E, padx = 1)
+    Label(group, text=active_table.read[i].tulp_e, bg=verv).grid(row=i, column=4, sticky=W+E, padx = 1)
+    Label(group, text=active_table.read[i].tulp_f, bg=verv).grid(row=i, column=5, sticky=W+E, padx = 1)
+    Label(group, text=active_table.read[i].tulp_g, bg=verv).grid(row=i, column=6, sticky=W+E, padx = 1)
+    Label(group, text=active_table.read[i].kuu_algul, bg=verv).grid(row=i, column=7, sticky=W+E, padx = 1)
+
         
     Label(group, text=active_table.read[i].tooner, bg=verv  ).grid(row=i, column=2, sticky=W+E)
     
