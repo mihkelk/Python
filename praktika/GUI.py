e96057712b6dd1a0f6dc5c9710a312832b788095
mihkel.kuslapuu <mihkel.kuslapuu@ameerika1.LEH.local>
2013-04-03 12:07:35 +0300

    Muudatused

--- praktika/GUI.py
+++ praktika/GUI.py
@@ -6,16 +6,23 @@ import Tkinter
 from Tkinter import *
 
 #active_table = tyhjad
-master = Tk()
+#master = Tk()
 
 class menyy:
     def __init__(self, master):
         self.master = master
-        menu = Menu(master)
-        menu.add_command(label = "t2is", command = self.t2is())
-        menu.add_command(label = "tyhi", command = self.tyhi())
-        nupp = Menubutton(master=master, menu = menu, text = "menuu")
-        nupp.pack()
+        mehneu = Menu(master)
+        #mehneu.add_command(label = "t2is", command = self.t2is())
+        #mehneu.add_command(label = "tyhi", command = self.tyhi())
+        #nupp = Menubutton(master=master, menu = mehneu, text = "menuu")
+        self.var = StringVar(self.master)
+        options = ["t2is", "tyhjad"]
+        option = OptionMenu(master, self.var, *options)
+        option.pack()
+        button = Button(master, text="OK", command=self.ok)
+        button.pack()
+        
+        #nupp.pack()
 
     def t2is(self):
         global t2is
@@ -24,7 +31,21 @@ class menyy:
     def tyhi(self):
         global tyhjad
         joonista_tabel(self.master, tyhjad)    
-        
+       
+    def ok(self):
+        #self.master.quit()
+        #self.master.
+        print "ok"
+        #global master, t2is, tyhjad, var
+        value = self.var.get()
+        print value
+        if value == "t2is":
+            #active_table = t2is
+            self.t2is()
+        elif value == "tyhjad":
+            #active_table = tyhjad
+            self.tyhi()
+            #print active_table    
         
 def joonista_tabel(master, active_table):
     master = master
@@ -32,19 +53,36 @@ def joonista_tabel(master, active_table):
     #global active_table
     group = Label(master)
     group.pack(padx=10, pady=0, fill=X)
+    
+    
     for i in range(active_table.k6rgus):
         
         if i %2:
             verv="white"
         else:
             verv="gray"
-        Label(group, text=active_table.read[i].printer, bg=verv).grid(row=i, sticky=W+E, padx = 1)
-        Label(group, text=active_table.read[i].tooner, bg=verv).grid(row=i, column=2, sticky=W+E, padx = 1)
-        Label(group, text=active_table.read[i].tulp_c, bg=verv).grid(row=i, column=3, sticky=W+E, padx = 1)
-        Label(group, text=active_table.read[i].tulp_e, bg=verv).grid(row=i, column=4, sticky=W+E, padx = 1)
-        Label(group, text=active_table.read[i].tulp_f, bg=verv).grid(row=i, column=5, sticky=W+E, padx = 1)
-        Label(group, text=active_table.read[i].tulp_g, bg=verv).grid(row=i, column=6, sticky=W+E, padx = 1)
-        Label(group, text=active_table.read[i].kuu_algul, bg=verv).grid(row=i, column=7, sticky=W+E, padx = 1)
+        
+        labels = [
+                  Label(group, text=active_table.read[i].printer, bg=verv)),
+                  Label(group, text=active_table.read[i].tooner, bg=verv),
+                  Label(group, text=active_table.read[i].tulp_c, bg=verv),
+                  Label(group, text=active_table.read[i].tulp_e, bg=verv),
+                  Label(group, text=active_table.read[i].tulp_f, bg=verv),
+                  Label(group, text=active_table.read[i].tulp_g, bg=verv),
+                  Label(group, text=active_table.read[i].kuu_algul, bg=verv)]
+        
+        for e in labels:
+            for columns in range(1,7)
+             labels[e].grid(row=i, column = columns, sticky=W+E, padx = 1)
+             
+            
+        #Label(group, text=active_table.read[i].printer, bg=verv).grid(row=i, sticky=W+E, padx = 1)
+        #Label(group, text=active_table.read[i].tooner, bg=verv).grid(row=i, column=2, sticky=W+E, padx = 1)
+        #Label(group, text=active_table.read[i].tulp_c, bg=verv).grid(row=i, column=3, sticky=W+E, padx = 1)
+        #Label(group, text=active_table.read[i].tulp_e, bg=verv).grid(row=i, column=4, sticky=W+E, padx = 1)
+        #Label(group, text=active_table.read[i].tulp_f, bg=verv).grid(row=i, column=5, sticky=W+E, padx = 1)
+        #Label(group, text=active_table.read[i].tulp_g, bg=verv).grid(row=i, column=6, sticky=W+E, padx = 1)
+        #Label(group, text=active_table.read[i].kuu_algul, bg=verv).grid(row=i, column=7, sticky=W+E, padx = 1)
     
             
         Label(group, text=active_table.read[i].tooner, bg=verv  ).grid(row=i, column=2, sticky=W+E)
@@ -52,18 +90,7 @@ def joonista_tabel(master, active_table):
     
     
     
-def ok():
-    print "ok"
-    global master, t2is, tyhjad, var
-    value = var.get()
-    print value
-    if value == "t2is":
-        active_table = t2is
-        joonista_tabel(master, active_table)
-    elif value == "tyhjad":
-        active_table = tyhjad
-        joonista_tabel(master, active_table)
-        print active_table
+
     #return active_table
     #master.quit()    
     
@@ -85,7 +112,7 @@ def tabelimenyy():
 
 #while True:
 menuu = menyy(Tk())
-tabelimenyy()
+#tabelimenyy()
 
 #joonista_tabel(master, t2is)
 #joonista_tabel(master, active_table)
